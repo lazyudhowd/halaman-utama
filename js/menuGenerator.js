366d99dfeae5dc8db34a89766cbb754687cb943b
lazyudhowd <154648790+lazyudhowd@users.noreply.github.com>
2024-02-28 15:02:33 +0700

    Update menuGenerator.js
    Menambah fitur
    Fitur ini juga akan menolak apabila ada objek yang tidak memiliki target url

--- js/menuGenerator.js
+++ js/menuGenerator.js
@@ -1,45 +1,58 @@
-/*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 function createMenu1(fObject, fTarget){
-	let target = document.getElementById(fTarget);
+	if(fObject.url){
+		let target = document.getElementById(fTarget);
 
-	let divCol = document.createElement("div");
-	divCol.setAttribute("class","col-lg-6 col-xxl-4 mb-5");
+		let divCol = document.createElement("div");
+		divCol.setAttribute("class","col-lg-6 col-xxl-4 mb-5");
 
-	let divCard = document.createElement("div");
-	divCard.setAttribute("class","card bg-light border-0 h-100");
+		let divCard = document.createElement("div");
+		divCard.setAttribute("class","card bg-light border-0 h-100");
 
-	let divCardBody = document.createElement("div");
-	divCardBody.setAttribute("class","card-body text-center p-4 p-lg-5 pt-0 pt-lg-0");
+		let divCardBody = document.createElement("div");
+		divCardBody.setAttribute("class","card-body text-center p-4 p-lg-5 pt-0 pt-lg-0");
 
-	let divFeature = document.createElement("div");
-	divFeature.setAttribute("class","feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4");
+		let divFeature = document.createElement("div");
+		divFeature.setAttribute("class","feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4");
 
-	let icon = document.createElement("i");
-	icon.setAttribute("class",fObject.icon.bootstrap);
+		let linkInIcon = document.createElement("a");
+		linkInIcon.setAttribute("class","iconLink");
+		linkInIcon.target = "_blank";
+		linkInIcon.title = fObject.judul;
+		linkInIcon.href = fObject.url;
 
-	let header2 = document.createElement("h2");
-	header2.setAttribute("class","fs-4 fw-bold");
-	header2.innerText = fObject.judul;
+		let icon = document.createElement("i");
+		icon.setAttribute("class",fObject.icon.bootstrap);
 
-	let paragraph = document.createElement("p");
-	paragraph.setAttribute("class","mb-0");
-	paragraph.innerText = fObject.deskripsi;
+		let header2 = document.createElement("h2");
+		header2.setAttribute("class","fs-4 fw-bold");
 
-	divFeature.appendChild(icon);
+		let linkInHeader = document.createElement("a");
+		let textInLinkHeader = document.createTextNode(fObject.judul);
+		linkInHeader.target = "_blank";
+		linkInHeader.setAttribute("class","headerLink");
+		linkInHeader.title = fObject.judul;
+		linkInHeader.href = fObject.url;
 
-	divCardBody.appendChild(divFeature);
-	divCardBody.appendChild(header2);
-	divCardBody.appendChild(paragraph);
+		let paragraph = document.createElement("p");
+		paragraph.setAttribute("class","mb-0");
+		paragraph.innerText = fObject.deskripsi;
 
-	divCard.appendChild(divCardBody);
-	divCol.appendChild(divCard);
+		linkInIcon.appendChild(icon);
 
-	target.appendChild(divCol);
+		linkInHeader.appendChild(textInLinkHeader);
+		header2.appendChild(linkInHeader);
+
+		divFeature.appendChild(linkInIcon);
+
+		divCardBody.appendChild(divFeature);
+		divCardBody.appendChild(header2);
+		divCardBody.appendChild(paragraph);
+
+		divCard.appendChild(divCardBody);
+		divCol.appendChild(divCard);
+
+		target.appendChild(divCol);
+	} else {
+		console.log("cant create menu because not have url");
+	}
 }
